9d5b7096430b1bce181726d8fed4cd02ce3d4fe7
oscar anillo <oscarani14@gmail.com>
2022-08-04 18:13:54 -0500

    add textComponent & avatarComponent

--- src/App.js
+++ src/App.js
@@ -4,8 +4,6 @@ import CardComponent from './components/card-component';
 function App() {
   return (
     <div className="App">
-      <h1>Oscar Anillo</h1> 
-      <p>Web UI Developer</p>
       <CardComponent />
     </div>
   );
